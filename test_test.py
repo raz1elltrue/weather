13d8d6bc99295a004a1e76fe4ab11efbe8900bf3
Raz1elL <akulyzhko@gamil.com>
2025-09-26 18:18:19 +0200

    Add unit tests for calculate_average function

---
+++ test_test.py
@@ -0,0 +1,24 @@
+import unittest
+from test import calculate_average
+
+class TestCalculateAverage(unittest.TestCase):
+    def test_average_of_positive_numbers(self):
+        self.assertEqual(calculate_average([1, 2, 3, 4, 5]), 3)
+
+    def test_average_of_negative_numbers(self):
+        self.assertEqual(calculate_average([-1, -2, -3]), -2)
+
+    def test_average_of_mixed_numbers(self):
+        self.assertEqual(calculate_average([-1, 0, 1]), 0)
+
+    def test_average_of_single_element(self):
+        self.assertEqual(calculate_average([10]), 10)
+
+    def test_average_of_empty_list(self):
+        self.assertEqual(calculate_average([]), 0)
+
+    def test_average_of_floats(self):
+        self.assertAlmostEqual(calculate_average([1.5, 2.5, 3.5]), 2.5)
+
+if __name__ == '__main__':
+    unittest.main()
